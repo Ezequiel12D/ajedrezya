42d8cc27b5c777f786d7d04ca91ef891e5d113c0
Ezequiel12D <Ezequiel.sosa.et26@gmail.com>
2024-08-22 21:06:49 -0300

    Base de datos

--- db/ajedrezya.sql
+++ db/ajedrezya.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-07-2024 a las 21:03:30
+-- Tiempo de generación: 23-08-2024 a las 02:05:42
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -40,10 +40,28 @@ CREATE TABLE `foro` (
 --
 
 INSERT INTO `foro` (`id`, `usuario`, `mensaje`, `fecha`, `parent_id`) VALUES
-(10, 'eze.135_', 'a', '2024-07-07 19:02:48', 0),
-(11, 'eze.135_', 'a', '2024-07-07 19:03:21', 0),
-(12, 'eze.135_', 'a', '2024-07-07 19:03:23', 0),
-(13, 'eze.135_', 'a', '2024-07-07 19:03:24', 0);
+(25, 'eze.135_', 'Abecedario', '2024-07-08 21:16:13', 0),
+(26, 'eze.135_', 'A', '2024-07-08 21:23:27', 25);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `email`, `password`) VALUES
+(1, 'eze.135_', 'ezesosa136@gmail.com', '$2y$10$o.KU5HQfIQ67SoO4Sn5d4OOpQy/knCl7NgJ7.QlBD8IbwnSRJUoTy');
 
 --
 -- Índices para tablas volcadas
@@ -56,6 +74,12 @@ ALTER TABLE `foro`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -63,7 +87,13 @@ ALTER TABLE `foro`
 -- AUTO_INCREMENT de la tabla `foro`
 --
 ALTER TABLE `foro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
